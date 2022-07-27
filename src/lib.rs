f66e08b57f41b68b34b15c95c79eeb620079fe8a
Colin Wee <2gbleh@gmail.com>
2022-07-26 18:04:30 -0700

    set OpenOptions create param; rework error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
-use std::io::Write;
 use std::ffi::{CStr, c_void};
 use std::fs::OpenOptions;
+use std::io::Write;
 use std::os::raw::c_char;
 
-#[no_mangle]
-pub extern "C" fn print_sslkeylogfile_line(_ssl: *const c_void, line: *const c_char)
-{
-    let mut log_file = OpenOptions::new()
-        .append(true)
-        .open("/tmp/openssl_keylogfile.txt");
-    if log_file.is_err() {
-        return;
-    }
-    let Ok(mut f) = log_file;
 
-    let slice = unsafe { CStr::from_ptr(line) };
-    let slice_r = slice.to_str();
-    if slice_r.is_err() {
-        return;
-    }
-    let Ok(printable_line) = slice_r;
-    writeln!(f, "{}", printable_line);
+fn log_sslkeylogfile_line_help(line_ptr: *const c_char) -> Option<()>
+{
+    let mut f = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("/tmp/openssl_keylogfile.txt")
+        .ok()?;
+
+    let line_str = unsafe { CStr::from_ptr(line_ptr) };
+    let line = line_str.to_str().ok()?;
+
+    writeln!(f, "{}", line).ok()?;
+    return Some(());
+}
+
+
+#[no_mangle]
+pub extern "C" fn log_sslkeylogfile_line(_ssl: *const c_void, line: *const c_char)
+{
+    log_sslkeylogfile_line_help(line);
 }
